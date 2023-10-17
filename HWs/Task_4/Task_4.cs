ea7c540f66d9add6f112dbd93a0827e8d572995b
Elijah_M <102888192+IiyaMezer@users.noreply.github.com>
2023-10-17 22:29:36 +0700

    Задания готовы

---
+++ HWs/Task_4/Task_4.cs
@@ -0,0 +1,31 @@
+﻿namespace Task_4
+{
+    internal class Task_4
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the 1st num: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the 2nd num: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the 3rd num: ");
+            int c = Convert.ToInt32(Console.ReadLine());
+
+
+
+            if (a > b || a > c)
+            {
+                Console.WriteLine(a);
+            }
+            else if (b > a || b > c)
+            {
+                 Console.WriteLine( b );
+            }
+            else if (c > a || c > b)
+            {
+                Console.WriteLine(c);
+            }
+            Console.ReadKey();
+        }
+    }
+}
